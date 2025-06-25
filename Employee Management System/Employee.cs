3eef26f4bf5d80ea734bfaddc529db9a06b19baf
MUGHILAN_OFFICIAL <mca211117@smvec.ac.in>
2025-06-25 06:23:41 +0530

    Add project files.

---
+++ Employee Management System/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+        public DateTime DateOfjoin { get; set; }
+
+
+        public void addData()
+        {
+            List<Employee> employee =new List<Employee>();
+
+            string whileValue = "y";
+            while (whileValue == "y")
+            {
+                Employee emp = new Employee();
+                Console.Write("Enter Employee ID: ");
+                emp.Id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Name: ");
+                emp.Name = Console.ReadLine();
+                Console.Write("Enter Department: ");
+                emp.Department = Console.ReadLine();
+                Console.Write("Enter the Salary: ");
+                emp.Salary =Convert.ToDouble( Console.ReadLine());
+                Console.Write("Enter the Date of join: ");
+                emp.DateOfjoin = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Add More(Y/N): ");
+                whileValue=Console.ReadLine().ToLower();
+
+            }
+        }
+    }
+}
---
+++ Employee Management System/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Employee_Management_System 
+{
+    public class Program {
+        public static void Main(string[] args){
+
+            Employee myObj = new Employee();
+            myObj.addData();
+        
+        }
+    }
+
+}
