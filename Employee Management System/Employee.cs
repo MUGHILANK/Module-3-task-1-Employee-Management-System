1fd35136a6cc3ffb0abfc6ec8df755505dea2ace
MUGHILAN_OFFICIAL <mca211117@smvec.ac.in>
2025-06-25 11:06:57 +0530

    After completed LINQ & Generics

--- Employee Management System/Employee.cs
+++ Employee Management System/Employee.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Employee_Management_System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Employee_Management_System
 {
     public class Employee
     {
+        
         public int Id { get; set; }
         public string Name { get; set; }
         public string Department { get; set; }
@@ -15,13 +18,14 @@ namespace Employee_Management_System
         public DateTime DateOfjoin { get; set; }
 
 
-        public void addData()
+        public static List<Employee> addData()
         {
-            List<Employee> employee =new List<Employee>();
+            List<Employee> employees =new List<Employee>();
 
             string whileValue = "y";
             while (whileValue == "y")
             {
+                Console.WriteLine("========== Adding Employee Details ==========");
                 Employee emp = new Employee();
                 Console.Write("Enter Employee ID: ");
                 emp.Id = Convert.ToInt32(Console.ReadLine());
@@ -34,10 +38,71 @@ namespace Employee_Management_System
                 Console.Write("Enter the Date of join: ");
                 emp.DateOfjoin = DateTime.Parse(Console.ReadLine());
 
-                Console.Write("Add More(Y/N): ");
+                employees.Add(emp);
+
+                Console.Write("Add More (Y/N): ");
                 whileValue=Console.ReadLine().ToLower();
 
             }
+            return employees;
+           
         }
+
+       public static void displayData(List<Employee> employees)
+        {
+            Console.WriteLine("========== Displaying Employee Details ==========");
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"ID:{emp.Id} Name:{emp.Name} Department: {emp.Department} Salary: {emp.Salary} Date of join: {emp.DateOfjoin}");
+            }
+        }
+
+        public static void filterDataUsingThreshold(List<Employee>employees)
+        {
+            Console.WriteLine("Enter the employees with salary above a threshold: ");
+            double getThresholdValue = Convert.ToDouble(Console.ReadLine());
+
+            var thresholdValue = employees.Where(a => a.Salary > getThresholdValue).ToList();
+
+            Console.WriteLine($"\n======= Employee threhold salary =======");
+            foreach (var emp in thresholdValue)
+            {
+                Console.WriteLine($"ID: {emp.Id}, Name: {emp.Name}, Salary: {emp.Salary}");
+            }
+
+        }
+
+        public static void groupByDepartment(List<Employee> employees) 
+        {
+            var getGroupedBy = employees.GroupBy(e => e.Department);
+            Console.WriteLine("\n====== Employees GroupBy ======");
+
+            foreach (var value in getGroupedBy)
+            {
+                Console.WriteLine($"\nDepartment: {value.Key}");
+                foreach (var emp in value)
+                {
+                    Console.WriteLine($"  ID: {emp.Id}, Name: {emp.Name}");
+                }
+            }
+
+        }
+        public static async void sortByDoj(List<Employee> employees)
+        {
+            await Task.Run(() =>
+            {
+                var sortBydoj = employees.OrderBy(e => e.DateOfjoin).ToList();
+                Console.WriteLine("\n======= Employees Sorted by Joining Date =======");
+                foreach (var emp in sortBydoj)
+                {
+                    Console.WriteLine($"Name: {emp.Name}, Joining Date: {emp.DateOfjoin.ToShortDateString()}");
+                }
+            });
+        }
+
+
+
     }
 }
+
+
--- Employee Management System/Program.cs
+++ Employee Management System/Program.cs
@@ -1,11 +1,50 @@
-﻿namespace Employee_Management_System 
+﻿using System.Collections.Generic;
+namespace Employee_Management_System 
 {
     public class Program {
         public static void Main(string[] args){
 
-            Employee myObj = new Employee();
-            myObj.addData();
-        
+            List<Employee> employees = Employee.addData();
+
+            string runWhile = "y";
+            Console.WriteLine("========== Employee Management System ==========");
+            while (runWhile =="y") {
+                Console.Write("1.To Display Full Data Table\n2.To Find Threshold Salary\nSelect the Option: ");
+                int switchData = Convert.ToInt32(Console.ReadLine());
+
+                switch (switchData)
+                {
+                    //case 1:
+                    //    //List<Employee> employees = Employee.addData();
+                    //    break;
+
+                    case 1:
+                        Employee.displayData(employees);
+                        break;
+
+                    case 2:
+                        Employee.filterDataUsingThreshold(employees);
+                        break;
+
+                    case 3:
+                        Employee.groupByDepartment(employees);
+                        break;
+
+                    default:
+                        Console.WriteLine("Select the Correct Answer: ");
+                        break;
+
+
+                }
+
+                Console.WriteLine("Press See agian (Y/N): ");
+                runWhile = Console.ReadLine();
+
+            }
+            
+            
+
+
         }
     }
 
